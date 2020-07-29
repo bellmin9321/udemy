69933c23df124f91d71f58efc32d9f070d3b5da0
bellmin9321 <bellmin9321@gmail.com>
2020-07-30 06:51:25 +0900

    login by php

---
+++ src/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <script>
+      password = prompt("비밀번호");
+      if(password == 111) {
+        document.write("success");
+      } else {
+        document.write("다시")
+      }
+    </script>
+  </body>
+</html>
